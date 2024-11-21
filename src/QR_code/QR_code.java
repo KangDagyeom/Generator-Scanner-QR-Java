a5538070d100a2cc3107cb0581bcb1b3a2306684
hungndweq <thang7781@gmail.com>
2024-11-21 10:31:15 +0700

    Upload files and folders

---
+++ src/QR_code/QR_code.java
@@ -0,0 +1,14 @@
+package QR_code;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Hyun
+ */
+public class QR_code {
+    
+}
